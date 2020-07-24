2bdcae93dc09a9c9a9ee9520425b58bbacde678a
giuse-dev <giuse.developing@gmail.com>
2020-07-25 00:38:18 +0200

    aggiunti metodi rest

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,11 +1,18 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.CustomerRepo;
@@ -50,4 +57,34 @@ public class CustomerController {
 		mv.setViewName("ShowCustomer.jsp");
 		return mv;
 	}
+	
+	@GetMapping("/customer/all")
+	@ResponseBody
+	public List<Customer> getAllCustomers(){
+		return repo.findAll();
+	}
+	
+	@GetMapping("/customer/{cid}")
+	@ResponseBody
+	public Customer getSingleCustomer(@PathVariable("cid") Integer cid) {
+		Customer customer = repo.findById(cid).orElse(null);
+		return customer;
+	}
+	
+	@PostMapping("/customer/create")
+	public void createCustomer(@RequestBody Customer customer) {
+		repo.save(customer);
+	}
+	
+	@DeleteMapping("/customer/delete/{cid}")
+	public void deleteCustomer(@PathVariable("cid") Integer cid) {
+		Customer customer = repo.findById(cid).orElseThrow(() -> new IllegalArgumentException("customer's id not found"));
+		repo.delete(customer);
+	}
+	
+	@PutMapping(path = "/customer/update" , consumes = {"application/json"})
+	public void updateCustomer(@RequestBody Customer customer) {
+		repo.save(customer);
+	}
+	
 }
